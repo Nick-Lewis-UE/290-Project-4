e75f4e8d70d0a9b167321985a3a120ba321643ca
Nick-Lewis-UE <nick_lewis@comcast.net>
2020-02-29 17:24:02 -0600

    Began implementing GomokuBoard constructors

--- src/GomokuBoard.java
+++ src/GomokuBoard.java
@@ -1,4 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class GomokuBoard extends AbstractBoard {
+
+    public GomokuBoard(List<List<AbstractPiece>> grid) {
+        this.grid = grid;
+    }
+
+    public GomokuBoard() {this(new ArrayList<ArrayList<AbstractPiece()>>);}
     public void addPiece() {}
     public void printBoard() {}
 }
