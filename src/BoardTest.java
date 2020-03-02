4fb16d8b5d3fb9a501a7208e26a0635f9d1b3972
Nick-Lewis-UE <nick_lewis@comcast.net>
2020-03-02 17:06:02 -0600

    Fixed misplaced tests

--- src/BoardTest.java
+++ src/BoardTest.java
@@ -1,2 +1,24 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 public class BoardTest {
+    private void testBoard(AbstractBoard b, ArrayList<ArrayList<Piece>> grid) {
+        Assert.assertTrue(grid.equals(b.getGrid()));
+
+    }
+
+    @Test
+    public void testConstructors() {
+        // Gomoku
+
+        // testing empty constructor
+        GomokuBoard b1 = new GomokuBoard();
+        testBoard(b1, new ArrayList<ArrayList<Piece>>() );
+
+        // Connect4
+        Connect4Board b2 = new Connect4Board();
+        testBoard(b2, new ArrayList<ArrayList<Piece>>());
+    }
 }
--- src/GameTest.java
+++ src/GameTest.java
@@ -4,21 +4,4 @@ import org.junit.Test;
 import java.util.ArrayList;
 
 public class GameTest {
-    private void testBoard(AbstractBoard b, ArrayList<ArrayList<Piece>> grid) {
-        Assert.assertTrue(grid.equals(b.getGrid()));
-
-    }
-
-    @Test
-    public void testConstructors() {
-        // Gomoku
-
-        // testing empty constructor
-        GomokuBoard b1 = new GomokuBoard();
-        testBoard(b1, new ArrayList<ArrayList<Piece>>() );
-
-        // Connect4
-        Connect4Board b2 = new Connect4Board();
-        testBoard(b2, new ArrayList<ArrayList<Piece>>());
-    }
 }
