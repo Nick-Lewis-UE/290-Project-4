35c11fdf199bc5d8ddca5973433b51201b3e5ec4
Nick-Lewis-UE <nick_lewis@comcast.net>
2020-03-03 22:31:58 -0600

    Added Player constructors and Piece instance variable

--- src/AbstractGame.java
+++ src/AbstractGame.java
@@ -1,7 +1,10 @@
 public abstract class AbstractGame {
-    public AbstractGame() {}
+    public AbstractGame() {
+    }
 
     protected AbstractBoard board;
+    protected Player p1;
+    protected Player p2;
 
     public void play() {}
 }
--- src/GomokuGame.java
+++ src/GomokuGame.java
@@ -1,2 +1,8 @@
 public class GomokuGame extends AbstractGame {
+
+    public GomokuGame() {
+        this.board = new GomokuBoard();
+        this.p1 = new Player("x");
+        this.p2 = new Player("o");
+    }
 }
