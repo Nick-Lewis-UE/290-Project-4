f754e5c54ae468d063b34e6bc4db456d0eee7f0e
Nick-Lewis-UE <nick_lewis@comcast.net>
2020-03-03 23:31:02 -0600

    Main started - setup done (to my understanding)

---
+++ src/Play.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Play {
+    public static void main (String[] args) {
+        Scanner scan = new Scanner(System.in);
+        AbstractGame game;
+        Player p1 = new Player("x");
+        Player p2 = new Player("o");
+
+        game = startup(scan);
+        game.getBoard().printBoard();
+
+        boolean win = false;
+        while (!win) {
+            game.play();
+        }
+
+    }
+
+    public static AbstractGame startup(Scanner scan) {
+        System.out.println("Which game would you like to play?");
+        System.out.println("Type 1 for Connect 4");
+        System.out.println("Type 2 for Gomoku");
+        String pick = scan.nextLine();
+
+        if (pick.equals("1")) {
+            return new Connect4Game();
+        } else if (pick.equals("2")) {
+            return new GomokuGame();
+        } else {
+            System.out.println("Invalid input, try again please.");
+            return startup(scan);
+        }
+    }
+}
